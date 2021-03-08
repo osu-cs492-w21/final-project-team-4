a7db75fefc94cdebe49ba5ac8ee41b3624f81336
fairhurt <55992872+fairhurt@users.noreply.github.com>
2021-03-08 11:32:29 -0800

    Fixed toolbar icons to be spaced out better.

--- app/src/main/java/com/example/android/steamnews/MainActivity.java
+++ app/src/main/java/com/example/android/steamnews/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,9 +25,30 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setIcon(R.drawable.steam_icon);
+
         Toolbar toolbarBottom = findViewById(R.id.bottom_toolbar);
        toolbarBottom.inflateMenu(R.menu.toolbar_menu_items);
-        //setSupportActionBar(toolbarBottom);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home_icon:
+                Log.d(TAG, "Setting Activity to Home");
+                return true;
+            case R.id.trending_icon:
+                Log.d(TAG, "Setting Activity to Trending");
+                return true;
+
+            case R.id.options_icon:
+                Log.d(TAG, "Setting Activity to Options");
+
+            case R.id.search_icon:
+                Log.d(TAG, "Setting Activity to Search");
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 
