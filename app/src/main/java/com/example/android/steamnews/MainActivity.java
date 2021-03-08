6910eb532c9fc503da7ae1974b3086f1f9bb1e82
fairhurt <55992872+fairhurt@users.noreply.github.com>
2021-03-08 10:31:16 -0800

    Added toolbar to the bottom w/ icons

--- app/src/main/java/com/example/android/steamnews/MainActivity.java
+++ app/src/main/java/com/example/android/steamnews/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.android.steamnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private RecyclerView rvArticleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.drawable.steam_icon);
+        Toolbar toolbarBottom = findViewById(R.id.bottom_toolbar);
+       toolbarBottom.inflateMenu(R.menu.toolbar_menu_items);
+        //setSupportActionBar(toolbarBottom);
+
     }
+
+
 }
