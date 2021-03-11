fc07ffe5c36a41220be89c10c4f3a6922283f9fe
fairhurt <55992872+fairhurt@users.noreply.github.com>
2021-03-11 13:02:05 -0800

    Fixed the button listeners to properly interact when they are clicked.

--- app/src/main/java/com/example/android/steamnews/MainActivity.java
+++ app/src/main/java/com/example/android/steamnews/MainActivity.java
@@ -22,35 +22,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //toolbar object for the upper toolbar (where the title is set)
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //set the icon next to the title to the Steam Icon
         getSupportActionBar().setIcon(R.drawable.steam_icon);
-
+        //create a toolbar object for the bottom toolbar
         Toolbar toolbarBottom = findViewById(R.id.bottom_toolbar);
-       toolbarBottom.inflateMenu(R.menu.toolbar_menu_items);
+        toolbarBottom.inflateMenu(R.menu.toolbar_menu_items);
+        //Listeners for clicks on the buttons on the bottom of the screen.
+        //These can be used to switch between activities
+        toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+              if(item.getItemId() == R.id.search_icon){
+                  Log.d(TAG, "Setting Activity to Search");
+              }else if(item.getItemId() == R.id.home_icon){
+                  Log.d(TAG, "Setting Activity to Home");
+              }else if(item.getItemId() == R.id.trending_icon){
+                  Log.d(TAG, "Setting Activity to Trending");
+              }else if(item.getItemId() == R.id.options_icon){
+                  Log.d(TAG, "Setting Activity to Options");
+              }
+              //else none of the id's match
+                return false;
+            }
 
+        });
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.home_icon:
-                Log.d(TAG, "Setting Activity to Home");
-                return true;
-            case R.id.trending_icon:
-                Log.d(TAG, "Setting Activity to Trending");
-                return true;
 
-            case R.id.options_icon:
-                Log.d(TAG, "Setting Activity to Options");
 
-            case R.id.search_icon:
-                Log.d(TAG, "Setting Activity to Search");
-            default:
-                return super.onOptionsItemSelected(item);
-        }
 
-    }
 
 
 }
